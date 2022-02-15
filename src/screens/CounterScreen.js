07cb86c590b29491ddde6049496305fe92b92658
Hamza <hamza@whunto.com>
2022-02-16 01:25:24 +0500

    Converted counter screen from useState to useReducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,20 +1,43 @@
-import React,{useState} from "react";
+import React,{useReducer} from "react";
 import {View, Text, Button, StyleSheet} from "react-native";
 
+const reducer = (state, action) =>{
+    switch(action.counterAction)
+    {
+        case 'increment':
+            return {...state, count: state.count + action.value} 
+        case 'decrement':
+            return {...state, count: state.count + action.value}
+        default:
+            return state;
+    }    
+}
+
 const CounterScreen = () =>{
-    let [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, {count:0});
+    const {count} = state;    
+    // let [counter, setCounter] = useState(0);
 
     return (
         <View>
-            <Button 
+            {/* <Button 
             title="Increase"
             onPress={()=>setCounter(counter+1)}
             />
             <Button 
             title="Decrease"
             onPress={()=>setCounter(counter-1)}
+            /> */}
+
+            <Button     
+            title="Increase"
+            onPress={()=>dispatch({counterAction:'increment',value:1})}
             />
-            <Text>Current Count: {counter}</Text>
+            <Button 
+            title="Decrease"
+            onPress={()=>dispatch({counterAction:'decrement',value:-1})}
+            />
+            <Text>Current Count: {count}</Text>
         </View>
     );
 }
